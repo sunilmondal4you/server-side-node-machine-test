8c22a1760ab7538c4e20af2ed9c35d33e3b41f7e
unknown <sunilmondal4you@gmail.com>
2019-10-12 08:59:49 -0700

    server side change

--- src/index.js
+++ src/index.js
@@ -1,24 +1,40 @@
 let express = require('express');
 let cors = require('cors')
+let bodyParser = require('body-parser')
+
 
 let app = express();
 
 // for cross origin request.
 app.use(cors());
 
+// parse application/json
+app.use(bodyParser.json());
+
+
+let list = [];
+
 
 /** GET RETURNS ALL PRODUCT */
 app.get("/", function(req, res) {
-    let output = [{productId: 1, productName : 'VIVO',  categoryName: 'Mobile', categoryId: 'V100'}];
-    res.json(output);
+    
+    res.json(list);
 });
 
 
 /** POST SAVE PRODUCT INTO DB */
 app.post("/", function(req, res){
+    let inputRequest = req.body;
+    list.push(inputRequest);
     
-    let output = {productId: 1, productName : 'VIVO',  categoryName: 'Mobile', categoryId: 'V100'};
-    res.json(output);
+    res.json(inputRequest);
+});
+
+/** POST SAVE PRODUCT INTO DB */
+app.put("/", function(req, res){
+    list = req.body;
+
+    res.json(list);
 });
 
 
